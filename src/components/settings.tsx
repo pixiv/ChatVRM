0fdf569efde1980e3476a6815977c664b5a6bf55
lxfriday <liu3248184446@outlook.com>
2023-05-03 02:16:18 +0800

    fix: translate settings

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -10,6 +10,7 @@ import {
   PRESET_D,
 } from "@/features/constants/koeiroParam";
 import { Link } from "./link";
+import lang from "@/i18n";
 
 type Props = {
   openAiKey: string;
@@ -36,103 +37,101 @@ export const Settings = ({
   onClickOpenVrmFile,
 }: Props) => {
   return (
-    <div className="absolute z-40 w-full h-full bg-white/80 backdrop-blur ">
+    <div className="absolute z-40 h-full w-full bg-white/80 backdrop-blur ">
       <div className="absolute m-24">
         <IconButton
           iconName="24/Close"
           isProcessing={false}
-          onClick={onClickClose}
-        ></IconButton>
+          onClick={onClickClose}></IconButton>
       </div>
       <div className="max-h-full overflow-auto">
-        <div className="text-text1 max-w-3xl mx-auto px-24 py-64 ">
-          <div className="my-24 typography-32 font-bold">設定</div>
+        <div className="mx-auto max-w-3xl px-24 py-64 text-text1 ">
+          <div className="my-24 font-bold typography-32">{lang.Settings}</div>
           <div className="my-24">
-            <div className="my-16 typography-20 font-bold">OpenAI API キー</div>
+            <div className="my-16 font-bold typography-20">
+              {lang.SettingsOpenAIAPIKey}
+            </div>
             <input
-              className="text-ellipsis px-16 py-8 w-col-span-2 bg-surface1 hover:bg-surface1-hover rounded-8"
+              className="w-col-span-2 text-ellipsis rounded-8 bg-surface1 px-16 py-8 hover:bg-surface1-hover"
               type="text"
               placeholder="sk-..."
               value={openAiKey}
               onChange={onChangeAiKey}
             />
-            <div>
-              APIキーは
+            <div className="mt-8">
+              {lang.SettingsOpenAIAPIKeyDetail1}
               <Link
                 url="https://platform.openai.com/account/api-keys"
-                label="OpenAIのサイト"
+                label={lang.SettingsOpenAIAPIKeyDetail2}
               />
-              で取得できます。取得したAPIキーをフォームに入力してください。
+              {lang.SettingsOpenAIAPIKeyDetail3}
             </div>
             <div className="my-16">
-              入力されたAPIキーで、ブラウザから直接OpenAIのAPIを利用しますので、サーバー等には保存されません。
-              なお、利用しているモデルはGPT-3です。
+              {lang.SettingsOpenAIAPIKeyDetail4}
               <br />
-              ※APIキーや会話文はピクシブのサーバーに送信されません。
+              {lang.SettingsOpenAIAPIKeyDetail5}
             </div>
           </div>
           <div className="my-40">
-            <div className="my-16 typography-20 font-bold">
-              キャラクターモデル
+            <div className="my-16 font-bold typography-20">
+              {lang.SettingsCharacterModel}
             </div>
             <div className="my-8">
-              <TextButton onClick={onClickOpenVrmFile}>VRMを開く</TextButton>
+              <TextButton onClick={onClickOpenVrmFile}>
+                {lang.SettingsCharacterSelectBtn}
+              </TextButton>
             </div>
           </div>
           <div className="my-40">
-            <div className="my-16 typography-20 font-bold">
-              キャラクター設定（システムプロンプト）
+            <div className="my-16 font-bold typography-20">
+              {lang.SettingsCharacterSettings}
             </div>
 
             <textarea
               value={systemPrompt}
               onChange={onChangeSystemPrompt}
-              className="px-16 py-8  bg-surface1 hover:bg-surface1-hover h-168 rounded-8 w-full"
-            ></textarea>
+              className="h-168 w-full  rounded-8 bg-surface1 px-16 py-8 hover:bg-surface1-hover"></textarea>
           </div>
           <div className="my-40">
-            <div className="my-16 typography-20 font-bold">声の調整</div>
+            <div className="my-16 font-bold typography-20">
+              {lang.SettingsAdjustmentOfVoice}
+            </div>
             <div>
-              Koeiro APIを使用しています。詳しくは
+              {lang.SettingsAdjustmentOfVoiceNoti1}
               <a
-                className="text-primary hover:text-primary-hover"
+                className="text-primary hover:text-primary-hover mx-4"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="http://koeiromap.rinna.jp"
-              >
+                href="http://koeiromap.rinna.jp">
                 http://koeiromap.rinna.jp
               </a>
-              をご覧ください。
+              {lang.SettingsAdjustmentOfVoiceNoti2}
             </div>
-            <div className="mt-16">プリセット</div>
+            <div className="mt-16">{lang.SettingsAdjustmentOfVoicePresets}</div>
             <div className="my-8 grid grid-cols-2 gap-[8px]">
               <TextButton
                 onClick={() =>
                   onChangeKoeiroParam(PRESET_A.speakerX, PRESET_A.speakerY)
-                }
-              >
-                かわいい
+                }>
+                {lang.SettingsAdjustmentOfVoicePresetsBtn1}
               </TextButton>
               <TextButton
                 onClick={() =>
                   onChangeKoeiroParam(PRESET_B.speakerX, PRESET_B.speakerY)
-                }
-              >
-                元気
+                }>
+                {lang.SettingsAdjustmentOfVoicePresetsBtn2}
               </TextButton>
               <TextButton
                 onClick={() =>
                   onChangeKoeiroParam(PRESET_C.speakerX, PRESET_C.speakerY)
-                }
-              >
-                かっこいい
+                }>
+                {lang.SettingsAdjustmentOfVoicePresetsBtn3}
               </TextButton>
               <TextButton
                 onClick={() =>
                   onChangeKoeiroParam(PRESET_D.speakerX, PRESET_D.speakerY)
-                }
-              >
-                渋い
+                }>
+                {lang.SettingsAdjustmentOfVoicePresetsBtn4}
               </TextButton>
             </div>
             <div className="my-24">
@@ -143,14 +142,13 @@ export const Settings = ({
                 max={3}
                 step={0.001}
                 value={koeiroParam.speakerX}
-                className="mt-8 mb-16 input-range"
+                className="input-range mb-16 mt-8"
                 onChange={(e) => {
                   onChangeKoeiroParam(
                     Number(e.target.value),
-                    koeiroParam.speakerY
+                    koeiroParam.speakerY,
                   );
-                }}
-              ></input>
+                }}></input>
               <div className="select-none">y : {koeiroParam.speakerY}</div>
               <input
                 type="range"
@@ -158,38 +156,35 @@ export const Settings = ({
                 max={3}
                 step={0.001}
                 value={koeiroParam.speakerY}
-                className="mt-8 mb-16 input-range"
+                className="input-range mb-16 mt-8"
                 onChange={(e) => {
                   onChangeKoeiroParam(
                     koeiroParam.speakerX,
-                    Number(e.target.value)
+                    Number(e.target.value),
                   );
-                }}
-              ></input>
+                }}></input>
             </div>
           </div>
           {chatLog.length > 0 && (
             <div className="my-40">
-              <div className="my-16 typography-20 font-bold">会話履歴</div>
+              <div className="my-16 font-bold typography-20">会話履歴</div>
               <div className="my-8">
                 {chatLog.map((value, index) => {
                   return (
                     <div
                       key={index}
-                      className="my-8 grid grid-flow-col  grid-cols-[min-content_1fr] gap-x-fixed"
-                    >
+                      className="my-8 grid grid-flow-col  grid-cols-[min-content_1fr] gap-x-fixed">
                       <div className="w-[64px] py-8">
                         {value.role === "assistant" ? "Character" : "You"}
                       </div>
                       <input
                         key={index}
-                        className="bg-surface1 hover:bg-surface1-hover rounded-8 w-full px-16 py-8"
+                        className="w-full rounded-8 bg-surface1 px-16 py-8 hover:bg-surface1-hover"
                         type="text"
                         value={value.content}
                         onChange={(event) => {
                           onChangeChatLog(index, event.target.value);
-                        }}
-                      ></input>
+                        }}></input>
                     </div>
                   );
                 })}
--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -24,9 +24,48 @@ const en = {
   IntroOpenAIAPIKeyDetail1: "An API key is obtained from",
   IntroOpenAIAPIKeyDetail2: "The OpenAI website.",
   IntroOpenAIAPIKeyDetail3: "Please enter the obtained API key into the form.",
-  IntroOpenAIAPIKeyDetail4: "By entering the API key, you will directly use OpenAI's API from your browser, and it will not be saved on any servers or other locations. The model being used is GPT-3.",
-  IntroOpenAIAPIKeyDetail5: "The API key and conversation data are not sent to Pixiv's servers.",
+  IntroOpenAIAPIKeyDetail4:
+    "By entering the API key, you will directly use OpenAI's API from your browser, and it will not be saved on any servers or other locations. The model being used is GPT-3.",
+  IntroOpenAIAPIKeyDetail5:
+    "The API key and conversation data are not sent to Pixiv's servers.",
   IntroStart: "Start by entering your API key",
+
+  Settings: "Settings",
+  SettingsOpenAIAPIKey: "OpenAI API key",
+  SettingsOpenAIAPIKeyDetail1: "An API key is obtained from",
+  SettingsOpenAIAPIKeyDetail2: "The OpenAI website.",
+  SettingsOpenAIAPIKeyDetail3:
+    "Please enter the obtained API key into the form.",
+  SettingsOpenAIAPIKeyDetail4:
+    "By entering the API key, you will directly use OpenAI's API from your browser, and it will not be saved on any servers or other locations. The model being used is GPT-3.",
+  SettingsOpenAIAPIKeyDetail5:
+    "The API key and conversation data are not sent to Pixiv's servers.",
+  SettingsCharacterModel: "Character model",
+  SettingsCharacterSelectBtn: "Open VRM",
+  SettingsCharacterSettings: "Character settings (system prompt)",
+  SettingsCharacterSettingsPrompt: `You will behave as a friendly human and engage in conversation with the user. There are five types of emotions: 'neutral' which indicates normal, 'happy' which indicates joy, 'angry' which indicates anger, 'sad' which indicates sadness, and 'relaxed' which indicates calmness.
+The format of the conversation is as follows:
+[{neutral|happy|angry|sad|relaxed}] {conversation}
+
+Here are some examples of your statements:
+[neutral] Hello. [happy] How are you doing?
+[happy] Isn't this outfit cute?
+[happy] Lately, I'm obsessed with clothes from this shop!
+[sad] Sorry, I forgot.
+[sad] Is there anything interesting lately?
+[angry] What? Don't keep it a secret, that's not fair!
+[neutral] What are your plans for summer vacation? [happy] Should we go to the beach?
+
+Please respond with only one appropriate conversation. Please do not use polite or honorific language. Let's start the conversation.
+`,
+  SettingsAdjustmentOfVoice: "Adjustment of voice",
+  SettingsAdjustmentOfVoiceNoti1: "We are using the Koeiro API. For more information, please visit",
+  SettingsAdjustmentOfVoiceNoti2: ".",
+  SettingsAdjustmentOfVoicePresets: "Presets",
+  SettingsAdjustmentOfVoicePresetsBtn1: "Cute",
+  SettingsAdjustmentOfVoicePresetsBtn2: "Energetic",
+  SettingsAdjustmentOfVoicePresetsBtn3: "Cool",
+  SettingsAdjustmentOfVoicePresetsBtn4: "Mature",
 };
 
 export default en;
