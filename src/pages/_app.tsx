a57e7e3dcbc19962b97905206e513673e6f58705
lxfriday <liu3248184446@outlook.com>
2023-05-03 05:06:51 +0800

    fix: remove

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "@charcoal-ui/icons";
-import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
