85d002b109f4efcf39581a29404e52d55fafc109
Harxxki <haruki4dev@gmail.com>
2023-09-03 19:30:15 +0900

    change page title

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,7 +1,7 @@
 import { buildUrl } from "@/utils/buildUrl";
 import Head from "next/head";
 export const Meta = () => {
-  const title = "ChatVRM";
+  const title = "AITuber Demo";
   const description =
     "Webブラウザだけで3Dキャラクターとの会話を、マイクやテキスト入力、音声合成を用いて楽しめます。キャラクター（VRM）の変更や性格設定、音声調整もできます。";
   const imageUrl = "https://pixiv.github.io/ChatVRM/ogp.png";
